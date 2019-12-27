01ca13a844323c40cae9e69b070d24db02198556
Eden Alem <edenberhanalem@gmail.com>
2019-12-27 15:35:46 +0300

    Updates

---
+++ precache-manifest.0129383e598554b40e47c2ec6467e970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537c6087e44d9a8248178633f7c1062f",
+    "url": "/DSC_React_Tryouts/index.html"
+  },
+  {
+    "revision": "3749c5da679f6306273a",
+    "url": "/DSC_React_Tryouts/static/css/main.54f47931.chunk.css"
+  },
+  {
+    "revision": "24bf8202b943983eb073",
+    "url": "/DSC_React_Tryouts/static/js/2.7939acd1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/DSC_React_Tryouts/static/js/2.7939acd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3749c5da679f6306273a",
+    "url": "/DSC_React_Tryouts/static/js/main.c699a49e.chunk.js"
+  },
+  {
+    "revision": "15b36924580dbd5897dd",
+    "url": "/DSC_React_Tryouts/static/js/runtime-main.b5252611.js"
+  }
+]);
